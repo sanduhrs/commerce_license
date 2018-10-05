801cabdd6586b5d36831be709844b9a7c4636c86
andyg5000 <andyg5000@808668.no-reply.drupal.org>
2018-10-05 18:29:50 +0100

    Issue #2577655 by andyg5000: Check for other licenses before revoking a role

--- modules/commerce_license_role/plugins/license_type/CommerceLicenseRole.class.php
+++ modules/commerce_license_role/plugins/license_type/CommerceLicenseRole.class.php
@@ -28,7 +28,7 @@ class CommerceLicenseRole extends CommerceLicenseBase  {
         if ($this->original->product_id && $this->product_id != $this->original->product_id) {
           $previous_role = $this->original->wrapper->product->commerce_license_role->value();
           if (isset($owner->roles[$previous_role])) {
-            unset($owner->roles[$previous_role]);
+            $this->revokeRole($owner, $previous_role);
             $save_owner = TRUE;
           }
         }
@@ -43,7 +43,7 @@ class CommerceLicenseRole extends CommerceLicenseBase  {
       elseif ($this->status > COMMERCE_LICENSE_ACTIVE) {
         // The owner of an inactive license must not have the role.
         if (isset($owner->roles[$role])) {
-          unset($owner->roles[$role]);
+          $this->revokeRole($owner, $role);
           $save_owner = TRUE;
         }
       }
@@ -55,5 +55,52 @@ class CommerceLicenseRole extends CommerceLicenseBase  {
     }
 
     parent::save();
+  }
+
+  /**
+   * Revokes the users role if there are no other active licenses.
+   *
+   * @param $owner object
+   *   The Drupal user object.
+   * @param $role integer
+   *   The numeric role value.
+   */
+  private function revokeRole($owner, $role) {
+    $revoke = TRUE;
+
+    // Load all of the users active licenses that have yet to expire.
+    $efq = new EntityFieldQuery();
+    $results = $efq->entityCondition('entity_type', 'commerce_license')
+      ->propertyCondition('type', 'role')
+      ->propertyCondition('uid', $owner->uid)
+      ->propertyCondition('status', COMMERCE_LICENSE_ACTIVE)
+      ->propertyCondition('license_id', $this->license_id, '!=')
+      ->execute();
+
+    // Loop through all of the users active licenses to see if they have another
+    // active license with the same role.
+    if (!empty($results)) {
+      $licenses = entity_load('commerce_license', array_keys($results['commerce_license']));
+      foreach ($licenses as $license) {
+        $license_wrapper = entity_metadata_wrapper('commerce_license', $license);
+        try {
+          $license_role = $license_wrapper->product->commerce_license_role->value();
+        }
+        catch (EntityMetadataWrapperException $ex) {
+          $license_role = FALSE;
+        }
+
+        // Set the revoke flag to false if there is a match so the user keeps their role.
+        if ($license_role == $role) {
+          $revoke = FALSE;
+        }
+      }
+    }
+
+    // Revoke the user role if it exists and there isn't another license for the same user
+    // and role.
+    if ($revoke && isset($owner->roles[$role])) {
+      unset($owner->roles[$role]);
+    }
   }
 }
